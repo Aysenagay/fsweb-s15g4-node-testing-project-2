d0765076f026e7d51dc0d68ebc41d5c0f802dfb1
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-06-03 16:13:53 +0300

    test hariç tamamlandı

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\Lenovo\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "C:\\Users\\Administrator\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
@@ -31,7 +31,7 @@ module.exports = {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  // coverageProvider: "babel",
+  coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
