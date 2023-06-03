d1db7e6a50c752734187f58a5f13f1353d90474f
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-06-03 15:38:05 +0300

    server çalıştırıldı

--- data/seeds/Appfsweb.js
+++ data/seeds/Appfsweb.js
@@ -1,13 +1,17 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+const defaultGorevler = [{ Adi: "Kilo Al", Aciklama: "Şişko Ol" }];
+const defaultTasklar = [
+  { Adi: "Evde Otur", Aciklama: "Evde patlamış mısır ye", GorevId: 1 },
+  { Adi: "Sağlıklı Beslen", Aciklama: "Meyve Ye", GorevId: 1 },
+];
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("Tasklar").truncate();
+  await knex("Gorevler").truncate();
+
+  await knex("Gorevler").insert(defaultGorevler);
+  await knex("Tasklar").insert(defaultTasklar);
 };
